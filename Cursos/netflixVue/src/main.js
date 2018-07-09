5e17e814ba93a75e5bb9bd13ace4bbf1d839727d
Matheus Graciano <mathgunz@gmail.com>
2018-07-09 18:09:18 -0300

    rotas utilizando o js

--- Cursos/netflixVue/src/main.js
+++ Cursos/netflixVue/src/main.js
@@ -1,10 +1,27 @@
 import Vue from 'vue'
-import App from './App.vue'
+//import App from './App.vue'
 import VueResource from 'vue-resource'
+
+import Home from './paginas/Home.vue'
+const Contato = {template: '<p>Pagina de Contato</p>'}
+const semPagina = {template: '<p>Pagina não encontrada</p>'}
 
 Vue.use(VueResource)
 
+const rotas = {
+  '/': Home,
+  '/contato': Contato
+}
+
 new Vue({
   el: '#app',
-  render: h => h(App)
+  data:{
+      rotaAtual: window.location.pathname
+  },
+  computed:{
+    exibeComponente (){
+      return rotas[this.rotaAtual] || semPagina
+    }
+  },
+  render (h) { return h(this.exibeComponente)}
 })
