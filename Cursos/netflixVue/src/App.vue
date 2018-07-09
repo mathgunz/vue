1bfd13f3b958f20a44808052c3cd4bfea84d49cb
Matheus Graciano <mathgunz@gmail.com>
2018-07-09 16:49:57 -0300

    componentes

--- Cursos/netflixVue/src/App.vue
+++ Cursos/netflixVue/src/App.vue
@@ -1,112 +1,28 @@
 <template>
 
   <div class="contain">
-    <div class="slider">
 
-      <h3>Ação</h3>
-      <span v-on:mouseover="scrollDireita()" v-on:mouseout="clearScroll()" class="handle handlePrev active">
-        <i class="fa fa-caret-left" aria-hidden="true"></i>
-      </span>
+    <categoria titulo="Comédia"></categoria>
+    <categoria titulo="Suspense"></categoria>
 
-      <div id="scroller" class="row">
-        <div class="row__inner">
-
-          <div class="gui-card">
-            <div class="gui-card__media">
-              <img class="gui-card__img" src="https://img1.ibxk.com.br/2015/11/12/12134915138723.jpg?w=700" alt=""  />
-            </div>
-            <div class="gui-card__details">
-              <div class="gui-card__title">
-                Assassin’s Creed
-              </div>
-            </div>
-          </div>
-
-          <div class="gui-card">
-            <div class="gui-card__media">
-              <img class="gui-card__img" src="https://img1.ibxk.com.br/2015/11/12/12134915138723.jpg?w=700" alt=""  />
-            </div>
-            <div class="gui-card__details">
-              <div class="gui-card__title">
-                Assassin’s Creed
-              </div>
-            </div>
-          </div>
-
-          <div class="gui-card">
-            <div class="gui-card__media">
-              <img class="gui-card__img" src="https://img1.ibxk.com.br/2015/11/12/12134915138723.jpg?w=700" alt=""  />
-            </div>
-            <div class="gui-card__details">
-              <div class="gui-card__title">
-                Assassin’s Creed
-              </div>
-            </div>
-          </div>
-
-          <div class="gui-card">
-            <div class="gui-card__media">
-              <img class="gui-card__img" src="https://img1.ibxk.com.br/2015/11/12/12134915138723.jpg?w=700" alt=""  />
-            </div>
-            <div class="gui-card__details">
-              <div class="gui-card__title">
-                Assassin’s Creed
-              </div>
-            </div>
-          </div>
-
-          <div class="gui-card">
-            <div class="gui-card__media">
-              <img class="gui-card__img" src="https://img1.ibxk.com.br/2015/11/12/12134915138723.jpg?w=700" alt=""  />
-            </div>
-            <div class="gui-card__details">
-              <div class="gui-card__title">
-                Assassin’s Creed
-              </div>
-            </div>
-          </div>
-
-          <div class="gui-card">
-            <div class="gui-card__media">
-              <img class="gui-card__img" src="https://img1.ibxk.com.br/2015/11/12/12134915138723.jpg?w=700" alt=""  />
-            </div>
-            <div class="gui-card__details">
-              <div class="gui-card__title">
-                Assassin’s Creed
-              </div>
-            </div>
-          </div>
-
-        </div>
-      </div>
-
-      <span v-on:mouseover="scrollDireita()" v-on:mouseout="clearScroll()" class="handle handleNext active">
-        <i class="fa fa-caret-right" aria-hidden="true"></i>
-      </span>
-    </div>
   </div>
 
 </template>
 
 <script>
+
+import Categoria from'./componentes/Categoria.vue';
+
 export default {
   name: 'app',
+  components:{
+    Categoria
+  },
   data () {
     return {
       msg: 'Welcome to Your Vue.js App',
       nomeProjeto:'Netflix com Vue',
       intervalo:null
-    }
-  },
-  methods:{
-    scrollDireita(){
-      this.intervalo = setInterval(function(){ document.getElementById('scroller').scrollLeft += 1 }  , 5);
-    },
-    scrollEsquerda(){
-      this.intervalo = setInterval(function(){ document.getElementById('scroller').scrollLeft -= 1 }  , 5);
-    },
-    clearScroll(){
-      clearInterval(this.intervalo);
     }
   }
 }
---
+++ Cursos/netflixVue/src/componentes/Filme.vue
@@ -0,0 +1,23 @@
+<template >
+
+  <div class="gui-card">
+    <div class="gui-card__media">
+      <img class="gui-card__img" v-bind:src="imagem" alt=""  />
+    </div>
+    <div class="gui-card__details">
+      <div class="gui-card__title">
+        {{titulo}}
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  props:['titulo', 'imagem']
+}
+</script>
+
+<style lang="scss">
+</style>
