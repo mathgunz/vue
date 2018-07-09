bce952699f477dcd64d916737b12088ec1fef73f
Matheus Graciano <mathgunz@gmail.com>
2018-07-09 18:19:48 -0300

    rotas framework

--- Cursos/netflixVue/src/main.js
+++ Cursos/netflixVue/src/main.js
@@ -2,26 +2,24 @@ import Vue from 'vue'
 //import App from './App.vue'
 import VueResource from 'vue-resource'
 
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
 import Home from './paginas/Home.vue'
 const Contato = {template: '<p>Pagina de Contato</p>'}
 const semPagina = {template: '<p>Pagina não encontrada</p>'}
 
 Vue.use(VueResource)
 
-const rotas = {
-  '/': Home,
-  '/contato': Contato
-}
+const routes = [
+  { path: '/', component: Home },
+  { path: '/contato', component: Contato }
+]
 
-new Vue({
-  el: '#app',
-  data:{
-      rotaAtual: window.location.pathname
-  },
-  computed:{
-    exibeComponente (){
-      return rotas[this.rotaAtual] || semPagina
-    }
-  },
-  render (h) { return h(this.exibeComponente)}
+const router = new VueRouter({
+  routes
 })
+
+const app = new Vue({
+  router
+}).$mount('#app')
